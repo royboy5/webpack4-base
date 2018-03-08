124ddff5c51bf800fe104a9132d486265c4206f3
Roy <roye19@gmail.com>
2018-03-07 22:06:31 -0500

    split up webpack config with webpack-merge

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,16 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: path.resolve(__dirname, '..', 'src/index.js')
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, '..', 'dist')
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, '..', 'dist'),
-        port: 5000
     },
     module: {
         rules: [
